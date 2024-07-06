48e4f3097dd9949ed50bc99ab01cf763978750bb
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-05 20:28:23 -0700

    Make equals calculate stored values, clear num2 value and move thecalculated total to num1 for future calculations

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const calculator = document.querySelector("#calculator");
 const display = document.querySelector(".display");
-
-let operator = ""
+const calculate = {
+    "+": function (a, b) { return a + b },
+    "-": function (a, b) { return a - b },
+    "*": function (a, b) { return a * b },
+    "/": function (a, b) { return a / b },
+};
+let operator = "";
 let num1 = 0;
 let num2 = 0;
 
@@ -19,7 +24,10 @@ calculator.addEventListener("click", (event) => {
     };
 
     if (event.target.innerText === "=") {
-        //do something
+        num2 = parseInt(display.textContent);
+        display.textContent = calculate[operator](num1, num2);
+        num1 = parseInt(display.textContent);
+        num2 = 0;
     };
     if (event.target.innerText === "C") {
         //do something
