9b3e1e45517dab818cea34c405a29d7a687fcb22
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-05 18:21:40 -0700

    Add event listener for id calculator

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 /*-------------------------------- Constants --------------------------------*/
 
+const calculator = document.querySelector("#calculator");
+
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
+
+calculator.addEventListener("click", (event) => {
+    console.log(event.target.innerText);
+
+    if (event.target.classList.contains("number")) {
+        //do something
+    }
+    if (event.target.innerText === "+") {
+        //do something
+    }
+    if (event.target.innerText === "-") {
+        //do something
+    }
+    if (event.target.innerText === "*") {
+        //do something
+    }
+    if (event.target.innerText === "/") {
+        //do something
+    }
+    if (event.target.innerText === "=") {
+        //do something
+    }
+    if (event.target.innerText === "C") {
+        //do something
+    }
+})
 
 /*-------------------------------- Functions --------------------------------*/
