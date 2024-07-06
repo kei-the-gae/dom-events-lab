ddd8e82b431ec8f70ea528a76b137d899b9d88ef
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-05 19:59:37 -0700

    Allow operator class other than C to commit current number to memory and clear for next input

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const calculator = document.querySelector("#calculator");
 const display = document.querySelector(".display");
 
+let operator = ""
+let num1 = 0;
+let num2 = 0;
+
 calculator.addEventListener("click", (event) => {
     console.log(event.target.innerText);
 
     if (event.target.classList.contains("number")) {
         display.textContent += event.target.innerText;
+        console.log(display.textContent);
     };
+    if (event.target.classList.contains("operator") && event.target.textContent != "C") {
+        num1 = (parseInt(display.textContent));
+        display.textContent = "";
+        console.log(num1);
+        console.log(typeof (num1));
+    }
     if (event.target.innerText === "+") {
         //do something
     };
